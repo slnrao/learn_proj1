1f52342429a582b3e7fe4766f54600e8c0a413a9
slnrao <sdrao.lnr.com@gmail.com>
2018-05-27 05:16:26 -0400

    ext_file_handel

--- prac2/prac_git1.py
+++ prac2/prac_git1.py
@@ -16,3 +16,19 @@ while True :
     f.write('\n')
 
 f.close()
+
+
+import os
+xsx = raw_input('enter the file name :')
+    if os.path.exists(xsx) :
+        if os.path.isfile(xsx) :
+            st =raw_input('enter the string to delete')
+            with open(xsx,r) as x:
+               d1=  x.read() # read returns in string format !!
+               d2=  x.replace(st,'')
+             print  d2 
+        else :
+            print xsx,'is not a file'
+    else :
+        print xsx,'no such file or directory'
+
